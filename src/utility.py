ba9361c455131721e97970b3839b73aff27f8852
sepsisAI_MIMIC_ED <ruchi.chauhan@siemens-healthineers.com>
2024-05-13 22:52:11 +0530

    add functions
    - create horizontal bar chart
    - split data before and after intervention

--- src/utility.py
+++ src/utility.py
@@ -21,3 +21,20 @@ def remove_rows_by_column_value(data, column_name, column_values):
     """
     cleaned_data = data[~data[column_name].isin(column_values)].copy()
     return cleaned_data
+
+
+def split_data_by_date(data, split_date="2028-03-01"):
+    before_df = df[df["uds_collection_date"] < split_date]
+    after_df = df[df["uds_collection_date"] >= split_date]
+    return before_df, after_df
+
+
+def create_barh_chart(
+    categories, values, xlabel="", ylabel="", title="", color=COLOR, figsize=(10, 6)
+):
+    plt.figure(figsize=figsize)
+    plt.barh(categories, values, color=color)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.show()
